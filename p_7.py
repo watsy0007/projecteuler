37fa18f75da7bbaa6c68a4f9a83b3d16fda87cd6
watsy0007 <watsy0007@gmail.com>
2018-11-26 15:12:35 +0800

    :sparkles: problem 7

---
+++ p_7.py
@@ -0,0 +1,31 @@
+# https://zh.wikipedia.org/wiki/質數定理
+from math import sqrt
+
+
+# https://zh.wikipedia.org/wiki/埃拉托斯特尼筛法
+def is_prime(n):
+    for i in range(2, int(sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
+def prime_by_index(number, index):
+    prime_index = 0
+    for (idx, v) in enumerate(range(2, number)):
+        if is_prime(v):
+            prime_index += 1
+            print(v, prime_index)
+            if index == prime_index:
+                return v
+
+
+def run():
+    print(prime_by_index(10_000_000, 10_002))  # 100 001st
+
+
+if __name__ == '__main__':
+    run()
+
+# 太慢了
+# python p_7.py  0.31s user 0.02s system 97% cpu 0.338 total
