af7b2eabb8e4ec513363db8f3a47db4722fe6be1
watsy0007 <watsy0007@gmail.com>
2018-11-26 19:48:05 +0800

    :sparkles: problem 14

--- p_14.py
+++ p_14.py
@@ -1,7 +1,8 @@
 def run():
     largest = 2
+    number = 2
     for i in range(2, 1_000_000):
-        chain_length = 1  # 1 and self
+        chain_length = 1
         sequence = i
         while sequence != 1:
             if sequence % 2 == 0:
@@ -11,11 +12,12 @@ def run():
             chain_length += 1
         if chain_length > largest:
             largest = chain_length
-    print(largest)
+            number = i
+    print(largest, number)
 
 
 if __name__ == '__main__':
     run()
 
 
-# python p_14.py  20.01s user 0.07s system 99% cpu 20.203 total
+# python p_14.py  19.41s user 0.04s system 99% cpu 19.502 total
